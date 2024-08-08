c6afb9c5f75b8dfd7f0f9146f86028a10257b6eb
Nhut Nguyen <shineforworks@gmail.com>
2024-08-08 16:23:36 +0700

    [ADD] Folder Cpp

---
+++ Cpp/Easygame/helloworld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+//lib for in/output operation
+#include<cstdio>
+
+//declare
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
