bdc9358bdd20ec0e7bff90d5db05b47a8336df3e
Nhut Nguyen <shineforworks@gmail.com>
2024-08-16 20:44:43 +0700

    [CHANGE] Test commit file forloop.cpp

--- Cpp/Easygame/forloop.cpp
+++ Cpp/Easygame/forloop.cpp
@@ -38,7 +38,6 @@ int main()
         case 9:
             cout << "nine" << endl;
             break;
-
         default:
             if(i%2==0) cout << "even" << endl;
             else  cout << "odd" << endl;
